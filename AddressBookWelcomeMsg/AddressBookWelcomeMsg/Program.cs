d2d867dd9d6b3d31de47ded75323cf4435af0e08
thakareshubh <shubhamthakare032@gmail.com>
2022-03-25 22:20:14 +0530

    Creat person information

--- AddressBookWelcomeMsg/AddressBookWelcomeMsg/Program.cs
+++ AddressBookWelcomeMsg/AddressBookWelcomeMsg/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections;
 
 namespace AddressBookWelcomeMsg
 {
+
     internal class Program
     {
+        /// <summary>
+        /// Creating Adress class with initialization
+        /// </summary>
+        public class Address
+        {
+            public string firstName, lastName, address, city, state, zip, phoneNumber, email;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the addresss book project");
+          
+            Console.WriteLine("Welcome to Address Book Program");
+            Address a = new Address();
+
+            //store information in addbook
+            ArrayList addbook = new ArrayList();
+            Console.WriteLine("Enter First Name ");
+            a.firstName = Console.ReadLine();
+            addbook.Add(a.firstName);
+
+            Console.WriteLine("Enter Last Name");
+            a.lastName = Console.ReadLine();
+            addbook.Add(a.lastName);
+
+            Console.WriteLine("Enter Address ");
+            a.address = Console.ReadLine();
+            addbook.Add(a.address);
+            Console.WriteLine("Enter city");
+            a.city = Console.ReadLine();
+            addbook.Add(a.city);
+
+            Console.WriteLine("Enter state");
+            a.state = Console.ReadLine();
+            addbook.Add(a.state);
+            Console.WriteLine("Enter zip");
+            a.zip = Console.ReadLine();
+            addbook.Add(a.zip);
+            Console.WriteLine("Enter Phone Number");
+            a.phoneNumber = Convert.ToString(Console.ReadLine());
+            addbook.Add(a.phoneNumber);
+            Console.WriteLine("Enter Email");
+            a.email = Convert.ToString(Console.ReadLine());
+            addbook.Add(a.email);
+
+            // New person information
+            Console.WriteLine("\nGiven Detail Are : \n");
+            Console.WriteLine("First Name = " + addbook[0] + "\nLastt Name = " + addbook[1] + "\nAddress = " + addbook[2] + "\nCity = " + addbook[3] + "\nState = " + addbook[4] + "\nZip = " + addbook[5] + "\nPhone Number = " + addbook[6] + "\nEmail = " + addbook[7]);
+
+
+
         }
     }
 }
