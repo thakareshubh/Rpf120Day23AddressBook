8254a9eb6f7245f8c4a6418a433798f730a04843
thakareshubh <shubhamthakare032@gmail.com>
2022-03-26 08:11:18 +0530

    Addes code for remove detail

--- AddressBookWelcomeMsg/AddressBookWelcomeMsg/Program.cs
+++ AddressBookWelcomeMsg/AddressBookWelcomeMsg/Program.cs
@@ -57,38 +57,9 @@ namespace AddressBookWelcomeMsg
                 string Name = Console.ReadLine();
                 if (firstName== Name)
                 {
-                    Console.WriteLine("Enter First Name ");
-                    firstName = Console.ReadLine();
-                    addbook.Add(firstName);
+                    addbook.RemoveRange(0, 7);
 
-                    Console.WriteLine("Enter Last Name");
-                    lastName = Console.ReadLine();
-                    addbook.Add(lastName);
-
-                    Console.WriteLine("Enter Address ");
-                    address = Console.ReadLine();
-                    addbook.Add(address);
-                    Console.WriteLine("Enter city");
-                    city = Console.ReadLine();
-                    addbook.Add(city);
-
-                    Console.WriteLine("Enter state");
-                    state = Console.ReadLine();
-                    addbook.Add(state);
-                    Console.WriteLine("Enter zip");
-                    zip = Console.ReadLine();
-                    addbook.Add(zip);
-                    Console.WriteLine("Enter Phone Number");
-                    phoneNumber = Convert.ToString(Console.ReadLine());
-                    addbook.Add(phoneNumber);
-                    Console.WriteLine("Enter Email");
-                    email = Convert.ToString(Console.ReadLine());
-                    addbook.Add(email);
-
-                    // New person information
-                    Console.WriteLine("\nGiven Detail Are : \n");
-                    Console.WriteLine("First Name = " + addbook[0] + "\nLastt Name = " + addbook[1] + "\nAddress = " + addbook[2] + "\nCity = " + addbook[3] + "\nState = " + addbook[4] + "\nZip = " + addbook[5] + "\nPhone Number = " + addbook[6] + "\nEmail = " + addbook[7]);
-
+                    Console.WriteLine("\n Remove {Name} detail");
 
                 }
 
